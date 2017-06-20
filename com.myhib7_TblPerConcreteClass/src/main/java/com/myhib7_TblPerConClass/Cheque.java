71f98fab36983a313c54849531fa012247676132
sky1508 <sky12345085@yahoo.com>
2017-06-20 23:12:58 +0530

    Added - Hibernate Inheritance - Table per concrete class

---
+++ com.myhib7_TblPerConcreteClass/src/main/java/com/myhib7_TblPerConClass/Cheque.java
@@ -0,0 +1,13 @@
+package com.myhib7_TblPerConClass;
+
+public class Cheque extends Payment {
+	private String ChequeType;
+	 
+	public String getChequeType() {
+		return ChequeType;
+	}
+ 
+	public void setChequeType(String chequeType) {
+		ChequeType = chequeType;
+	}
+}
---
+++ com.myhib7_TblPerConcreteClass/src/main/java/com/myhib7_TblPerConClass/CreditCard.java
@@ -0,0 +1,13 @@
+package com.myhib7_TblPerConClass;
+
+public class CreditCard extends Payment {
+	private String CreditCardType;
+	 
+	public String getCreditCardType() {
+		return CreditCardType;
+	}
+ 
+	public void setCreditCardType(String creditCardType) {
+		CreditCardType = creditCardType;
+	}
+}

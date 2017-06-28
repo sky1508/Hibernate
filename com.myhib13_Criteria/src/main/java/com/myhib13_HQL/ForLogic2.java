14de184f8a6242c53c7285dc6bd2e76f4affddf8
sky1508 <sky12345085@yahoo.com>
2017-06-28 23:28:53 +0530

    Added Hibernate - Criteria Query & Criteria Query with Order

---
+++ com.myhib13_Criteria/src/main/java/com/myhib13_HQL/ForLogic2.java
@@ -0,0 +1,50 @@
+package com.myhib13_HQL;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+public class ForLogic2 {
+	/*Criteria Query with Additional condition such as asc sort using order class*/
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate/hibernate.cfg.xml"); 
+
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();		
+
+		Criteria crit = session.createCriteria(Product.class);
+		Criterion cn = Restrictions.gt("price",new Double(17000));
+		crit.add(cn);
+		
+		crit.addOrder(Order.asc("price"));
+		List l=crit.list();
+		
+		System.out.println("List total size..._"+l.size());
+		Iterator it=l.iterator();
+
+		while(it.hasNext())
+		{
+			Product p=(Product)it.next();
+			System.out.println(p.getProductId());
+			System.out.println(p.getProName());
+			System.out.println(p.getPrice());
+			System.out.println();
+		}
+
+		
+		session.close();
+		factory.close();
+
+
+	}
+
+}

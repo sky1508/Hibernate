04411efabaedaff7c110ed03405b9de6362d9024
sky1508 <sky12345085@yahoo.com>
2017-06-29 23:24:39 +0530

    Added - Hibernate - Servlet Example Pagination

---
+++ Pagination/src/com/myhib15/Product.java
@@ -0,0 +1,37 @@
+package com.myhib15;
+
+public class Product implements java.io.Serializable{
+	 
+	private static final long serialVersionUID = 1L;
+ 
+	private int productId;
+	private String proName;
+	private double price;
+ 
+	public void setProductId(int productId)
+	{
+	    this.productId = productId;
+	}
+	public int getProductId()
+	{
+	    return productId;
+	}
+ 
+	public void setProName(String proName)
+	{
+	    this.proName = proName;
+	}
+	public String getProName()
+	{
+	    return proName;
+	}
+ 
+	public void setPrice(double price)
+	{
+	    this.price = price;
+	}
+	public double getPrice()
+	{
+	    return price;
+	}
+}

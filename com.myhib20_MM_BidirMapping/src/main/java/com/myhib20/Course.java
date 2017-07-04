e34f2aa4aac3a463a3f7c83400860a3f448ffe62
sky1508 <sky12345085@yahoo.com>
2017-07-04 22:39:42 +0530

    Added Hibernate Many to Many Mapping pgm

---
+++ com.myhib20_MM_BidirMapping/src/main/java/com/myhib20/Course.java
@@ -0,0 +1,46 @@
+package com.myhib20;
+
+import java.util.Set;
+
+@SuppressWarnings("rawtypes")
+public class Course {
+	private int courseId;
+	private String courseName;
+	private int duration;
+ 
+	
+	private Set students;
+ 
+	public int getCourseId() {
+		return courseId;
+	}
+ 
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+ 
+	public String getCourseName() {
+		return courseName;
+	}
+ 
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+ 
+	public int getDuration() {
+		return duration;
+	}
+ 
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+ 
+	public Set getStudents() {
+		return students;
+	}
+ 
+	public void setStudents(Set students) {
+		this.students = students;
+	}	
+ 
+}
